9f2a70c0480ee189a9fe80067cb912e9bdfb83d7
Mauro Gori <maurogoripro@gmail.com>
2025-05-20 12:39:38 +0200

    implemented various funcs

--- Gori_func.cpp
+++ Gori_func.cpp
@@ -1,8 +1,41 @@
-#ifndef _GORI__
-#define _GORI__
-
-
-
-
-
-#endif
+bool insertPoly(vector<Function *> &f){
+}
+//k * x ^ e
+bool insertPow(vector<Function *> &f){
+	double k;
+	double e;
+	Power* p;
+	p = new Power;
+	cout<<"Power Function creation wizard"<<endl;
+	cout<<"insert k coefficent"<<endl;
+	cin>>k;
+	cout<<"insert e coefficent"<<endl;
+	cin>>e;
+	p->SetPower(k, e);
+	return true;
+}
+	
+bool insertExp(vector<Function *> &f){
+	double k;
+	double b;
+	double c;
+	Exponential* e;
+	e = new Exponential;
+	cout<<"insert k coefficent"<<endl;
+	cin>>k;
+	cout<<"insert c coefficent"<<endl;
+	cin>>c;
+	while(1){
+		cout<<"insert b coefficent"<<endl;
+		cin>>b;
+		if(b > 0){
+			break;
+		}
+		else{
+			cout<<"[ ERROR ] b coefficent must be > 0"<<endl;
+		}
+	}
+	e->SetExponential(k, b, c);
+	return true;
+	
+}
